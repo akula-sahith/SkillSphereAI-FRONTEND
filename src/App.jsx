d7cca2fc06c059a58661ddfd164510ec89487330
Sahith Akula <akulasahith268@gmail.com>
2025-08-22 15:10:40 +0530

    Some files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landingpage";
+import StudentDashboard from "./components/StudentDashboard";
+import StartLearning from "./components/StartLearning";
+import CoursePage from "./components/CoursePage";
 
 
 const App = () => {
@@ -8,6 +11,9 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/StudentDashboard" element={<StudentDashboard/>}/>
+        <Route path="/start-learning" element={<StartLearning/>}/>
+        <Route path="/course/:coursename" element={<CoursePage/>}/>
       </Routes>
     </Router>
   );
